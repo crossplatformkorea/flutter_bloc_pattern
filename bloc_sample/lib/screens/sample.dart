7d8833b60a12321cea1671fa5c37f291a33bda3d
hyochan <dooboolab@gmail.com>
2018-10-30 05:59:19 +0900

    bloc_sampe with interited widget.

--- bloc_sample/lib/screens/sample.dart
+++ bloc_sample/lib/screens/sample.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '../blocs/bloc_provider.dart';
 import '../blocs/sample_bloc.dart';
 
 class Sample extends StatelessWidget {
@@ -9,23 +10,25 @@ class Sample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sample'),
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(height: 12.0),
-            Text('SampleChild1'),
-            SampleChild1(bloc),
-            Container(height: 12.0),
-            Text('SampleChild2'),
-            SampleChild2(bloc),
-            Container(height: 12.0),
-            Text('SampleChild3'),
-            SampleChild3(bloc),
-          ],
+    return BlocProvider(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Sample'),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Container(height: 12.0),
+              Text('SampleChild1'),
+              SampleChild1(),
+              Container(height: 12.0),
+              Text('SampleChild2'),
+              SampleChild2(),
+              Container(height: 12.0),
+              Text('SampleChild3'),
+              SampleChild3(),
+            ],
+          ),
         ),
       ),
     );
@@ -33,13 +36,10 @@ class Sample extends StatelessWidget {
 }
 
 class SampleChild1 extends StatelessWidget {
-  SampleChild1(this.bloc);
-  final SampleBloc bloc;
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: bloc.message,
+      stream: BlocProvider.of(context).sampleBloc.message,
       builder: (context, snapshot) {
         return Container(
           child: Text(snapshot.data ?? ''),
@@ -50,13 +50,10 @@ class SampleChild1 extends StatelessWidget {
 }
 
 class SampleChild2 extends StatelessWidget {
-  SampleChild2(this.bloc);
-  final SampleBloc bloc;
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: bloc.message,
+      stream: BlocProvider.of(context).sampleBloc.message,
       builder: (context, snapshot) {
         return Container(
           child: Text(snapshot.data ?? ''),
@@ -67,18 +64,15 @@ class SampleChild2 extends StatelessWidget {
 }
 
 class SampleChild3 extends StatelessWidget {
-  SampleChild3(this.bloc);
-  final SampleBloc bloc;
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: bloc.message,
+      stream: BlocProvider.of(context).sampleBloc.message,
       builder: (context, snapshot) {
         return Container(
           width: 200.0,
           child: TextField(
-            onChanged: (String str) => bloc.setMessage(str),
+            onChanged: (String str) => BlocProvider.of(context).sampleBloc.setMessage(str),
           ),
         );
       },
